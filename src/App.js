b6962abb87c642eea57f533918b029cfc4e04066
Smitch5777 <Stevenmit123@gmail.com>
2025-05-31 12:29:00 -0600

    routing changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/phenokinetics">
       <div className="App">
         <Navigation />
         <main className="main-content">
